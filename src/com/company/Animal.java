8c0625df24dc3170c9f51a7f2348021356ccad16
Maciej <maciejweltrowski@gmail.com>
2020-04-21 19:17:18 +0200

    Zadanie 0

---
+++ src/com/company/Animal.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.io.File;
+
+public class Animal {
+    String species;
+    Double weight;
+    String name;
+    File pic;
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,29 @@ package com.company;
 
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+
+        Phone phone1 = new Phone();
+        phone1.producer = "Apple";
+        phone1.model = "Iphone11";
+        phone1.screenSize = 6.1;
+        phone1.isAndroid = false;
+
+        Animal dog = new Animal();
+        dog.species = "dog";
+        dog.weight = 20.0;
+        dog.name = "Azor";
+
+        Human me = new Human();
+        me.firstName = "Maciej";
+        me.lastName = "Weltrowski";
+        me.phone=phone1;
+        me.pet=dog;
+
+        System.out.println(me.firstName);
+        System.out.println(me.phone.model);
+        System.out.println(me.pet.name);
+
     }
 }
